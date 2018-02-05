38b8e597d5842d973105ea01d619fb4876a4cb61
James Bond <james@Jamess-MacBook-Pro.local>
2018-02-06 00:54:47 +0200

    first

---
+++ Desktop/testLib/Classes/testLibFile.swift
@@ -0,0 +1,16 @@
+//
+//  testLibFile.swift
+//  Pods-testLib_Example
+//
+//  Created by James Bond on 06/02/2018.
+//
+
+import Foundation
+
+public class testClass {
+    
+    public func testAccess() -> String {
+        return "success"
+    }
+    
+}
